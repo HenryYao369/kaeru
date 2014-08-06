c255e3fe3dd5b223bcc976741c7ed61b947d68aa
bennn <greenman.b@husky.neu.edu>
2014-08-05 20:11:57 -0400

    about dispatch

--- kaeru/views.py
+++ kaeru/views.py
@@ -1,9 +1,30 @@
 from django.conf import settings
+from django.http import Http404
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+import os
+
+# These pages should live in 'kaeru/templates/about/'
+ABOUT_PAGES = [
+    'about',
+    'documentation',
+    'download',
+    'faq',
+    'news',
+    'people',
+    'publications',
+    'support',
+    'tutorial',
+]
+def about(request, pagename='about'):
+    # Search for a matching template page, raise 404 if none found.
+    if pagename in ABOUT_PAGES:
+        return render_to_response('about/%s.html' % pagename, {})
+    else:
+        raise Http404
 
 def index(request):
-    return render_to_response('index.html', {'index': True})
+    return render_to_response('index.html', {})
 
 def login(request):
     if request.method == "POST":
