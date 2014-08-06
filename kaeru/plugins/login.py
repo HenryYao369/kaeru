15cddb8db5d095b1d5601cce37b04109266ca4ad
bennn <greenman.b@husky.neu.edu>
2014-08-06 14:34:11 -0400

    removed unused plugins folder

--- kaeru/plugins/login.py
+++
@@ -1,9 +0,0 @@
-
-def shortname(): 
-    return "login"
-
-def description():
-    return "Log in to your account"
-
-def input(request):
-    return django.shortcuts.render_to_response('plugins/login.html', {})
