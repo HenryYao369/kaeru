08eb9ae08007c5fba0db9cd8dee3cfc17c7678b0
bennn <greenman.b@husky.neu.edu>
2014-08-06 03:52:02 -0400

    simple login view tests

--- kaeru/tests.py
+++ kaeru/tests.py
@@ -1,6 +1,58 @@
 from django.test import TestCase
+from django.utils import timezone
+from kaeru.models import User
+
+
+class LoginTest(TestCase):
+
+    def setUp(self):
+        User(name="anon",
+             favorite_color="anon",
+             email="anon@anon.anon",
+             create_date=timezone.now(),
+             login_date=timezone.now()
+         ).save()
+
+    def test_login_nouser(self):
+        response = self.client.post('/login/', {})
+        self.assertEqual(200, response.status_code)
+        # KLUDGE
+        self.assertContains(response, "Please enter your username.")
+
+    def test_login_nopass(self):
+        response = self.client.post('/login/', {'username': 'ben'})
+        self.assertContains(response, "Please enter your password.")
+
+    def test_login_invalid_username(self):
+        username = ""
+        password = ""
+        response = self.client.post('/login/', {'username': username,
+                                                'password': password})
+        self.assertContains(response, "not found.")
+
+    def test_login_invalid_password(self):
+        username = User.objects.get().name
+        password = ""
+        response = self.client.post('/login/', {'username': username,
+                                                'password': password})
+        self.assertContains(response, "Invalid password")
+
+    def test_login_success(self):
+        user = User.objects.get()
+        response = self.client.post('/login/', {'username': user.name,
+                                                'password': user.favorite_color})
+        self.assertContains(response, "Welcome")
+
 
 class UrlsTest(TestCase):
+
+    def test_login_page_exists(self):
+        """
+        Check that there is a login page
+        """
+        response = self.client.get('/login/')
+        self.assertEqual(200, response.status_code)
+
     def test_index_exists(self):
         """
         Check that index (landing) page exists
@@ -8,7 +60,7 @@ class UrlsTest(TestCase):
         response = self.client.get('/')
         self.assertEqual(200, response.status_code)
 
-    def test_about_pages_exist(self):
+    def test_about_exists(self):
         """
         Check that each about page exists
         """
@@ -17,7 +69,7 @@ class UrlsTest(TestCase):
             response = self.client.get('/about/%s/' % pagename)
             self.assertEqual(200, response.status_code)
 
-    def test_about_page_404(self):
+    def test_about_404(self):
         """
         Raise an arbitrary 404 in the views.about function
         """
@@ -26,16 +78,9 @@ class UrlsTest(TestCase):
         response = self.client.get('/about/%s/' % pagename)
         self.assertEqual(404, response.status_code)
 
-    def test_about_page_default(self):
+    def test_about_default(self):
         """
         Check that default about page exists
         """
         response = self.client.get('/about/')
-        self.assertEqual(200, response.status_code)
-
-    def test_login_page_exists(self):
-        """
-        Check that there is a login page
-        """
-        response = self.client.get('/login/')
         self.assertEqual(200, response.status_code)
