dd703fbd08d6a69a69d1547f982339188e7f9f61
Ben Greenman <greenman.b@husky.neu.edu>
2014-08-07 20:03:55 -0400

    login using django auth

--- kaeru/views.py
+++ kaeru/views.py
@@ -1,9 +1,9 @@
 from django.conf import settings
+from django.contrib.auth import authenticate
 from django.http import Http404
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
-
-from kaeru.models import User
+from django.core.context_processors import csrf
 
 import os
 
@@ -31,28 +31,23 @@ def index(request):
     return render_to_response('index.html', {})
 
 def login(request):
+    cookie = {}
+    cookie.update(csrf(request)) # Required for csrf form protection
     if request.method == "POST":
         # Try to log the user in, else show the 'create account' page
         username = request.POST.get('username', None)
         password = request.POST.get('password', None)
-        # Error fallback
-        if username is None:
-            errormsg = "Please enter your username."
-        elif password is None:
-            errormsg = "Please enter your password."
-        elif not User.objects.filter(name=username):
-            errormsg = "Username '%s' not found." % username
-        elif not User.objects.filter(name=username, favorite_color=password):
-            # TODO replace with passwordhash
-            errormsg = "Invalid password"
+        user = authenticate(username=username, password=password)
+        if user is None:
+            cookie['error_message'] = "Login failed."
+        elif not (user.is_active):
+            cookie['error_message'] = "Username/Password combo is valid, but the account has been disabled."
         else:
-            # Find user, TODO save user to session.
-            # TODO httpresponseredirect
-            errormsg = None
-            user = User.objects.filter(name=username,favorite_color=password)[0]
-            return render_to_response('login.html', {"user": user})
-        return render_to_response('login.html', {"error_message": errormsg})
+            # User is valid, active, and authenticated
+            cookie['error_message'] = None
+            cookie['user'] = user
+        return render_to_response('login.html', cookie)
     else:
         # Show the login page
-        return render_to_response('login.html', {})
+        return render_to_response('login.html', cookie)
 
