f3c0a9e45330aea4854eda73dc69ed4c1a7f4874
Bo Yuan Zhou <bz88@cornell.edu>
2015-03-22 22:36:59 -0400

    added project to user relations

--- kaeru/migrations/0001_initial.py
+++ kaeru/migrations/0001_initial.py
@@ -2,11 +2,13 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
@@ -14,8 +16,10 @@ class Migration(migrations.Migration):
             name='Project',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('hidden', models.BooleanField()),
+                ('hidden', models.BooleanField(default=False)),
                 ('create_date', models.DateTimeField(verbose_name=b'date created')),
+                ('contributors', models.ManyToManyField(related_name='project_contributors', to=settings.AUTH_USER_MODEL)),
+                ('creator', models.ForeignKey(related_name='project_creator', default=None, to=settings.AUTH_USER_MODEL)),
             ],
             options={
             },
--- kaeru/models.py
+++ kaeru/models.py
@@ -1,13 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 VERSIONMAJOR=1
 VERSIONMINOR=0
 VERSION=('%d.%d' % (VERSIONMAJOR, VERSIONMINOR))
 
-# use django.contrib.auth.models.User
-
 class Project(models.Model):
-    # creator = models.ForeignKey(User) # Original creator
-    # contributors = models.ManyToManyField(User) # Contributors to the project
-	hidden = models.BooleanField() # Public/Private Visibility
+	creator = models.ForeignKey(User, default=None, related_name="project_creator") # Original creator
+	contributors = models.ManyToManyField(User, related_name="project_contributors") # Contributors to the project
+	hidden = models.BooleanField(default=False) # Public/Private Visibility
 	create_date = models.DateTimeField('date created') # Creation date
