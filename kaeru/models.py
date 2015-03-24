46c2ccc5060c3a56463fe0fa486aa5faf2c6ed21
Bo Yuan Zhou <bz88@cornell.edu>
2015-03-24 00:34:49 -0400

    added tests for projects model

--- kaeru/models.py
+++ kaeru/models.py
@@ -7,7 +7,13 @@ VERSION=('%d.%d' % (VERSIONMAJOR, VERSIONMINOR))
 
 # Backing model for a user-created project
 class Project(models.Model):
-	creator = models.ForeignKey(User, default=None, related_name="project_creator") # Original creator
-	contributors = models.ManyToManyField(User, related_name="project_contributors") # Contributors to the project
-	hidden = models.BooleanField(default=False) # Public/Private Visibility
-	create_date = models.DateTimeField('date created') # Creation date
+    name = models.CharField(max_length=200) # Name of the project
+    creator = models.ForeignKey(User, default=None, related_name="project_creator") # Original creator
+    contributors = models.ManyToManyField(User, related_name="project_contributors") # Contributors to the project
+    hidden = models.BooleanField(default=False) # Public/Private Visibility
+    create_date = models.DateTimeField('date created') # Creation date
+
+    # String representation of the project model, which is its name
+    # () -> String
+    def __str__(self):
+        return self.name
