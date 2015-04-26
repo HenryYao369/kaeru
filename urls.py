918555f4f3bc8deb5a3a4961c8610d27942bfb1a
HenryYAO <hengzhi369@gmail.com>
2015-04-26 13:30:04 -0400

    URL revise -- to show admin page.
    comment out one url config to show admin page.

--- urls.py
+++ urls.py
@@ -17,5 +17,5 @@ urlpatterns = patterns('',
     url(r'^signup/$' , 'kaeru.views.signup_view'),
     url(r'^people/$' , 'kaeru.views.people_view'),
     url(r'^projects/$' , 'kaeru.views.projects_view'), 
-    url(r'^tutorial/$' , 'kaeru.views.tutorial_view'), 
+    # url(r'^tutorial/$' , 'kaeru.views.tutorial_view'),  # comment out this line to go to admin site.
 )
